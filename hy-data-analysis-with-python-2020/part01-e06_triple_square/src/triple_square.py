1ac01917d69ade26bb07ec125c093d1852884948
daniu <dbrisaro@gmail.com>
2020-07-23 21:00:36 -0300

    Actualizo ejercicios 5,6 y 7

--- hy-data-analysis-with-python-2020/part01-e06_triple_square/src/triple_square.py
+++ hy-data-analysis-with-python-2020/part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
+# Write two functions: triple and square. Function triple multiplies its parameter by three.
+# Function square raises its parameter to the power of two. For example,
+# we have equalities triple(5)==15 and square(5)==25.
+# Now modify this for loop so that it stops iteration when the square of a value
+# is larger than the triple of the value, without printing anything in the last iteration.
+
+def triple(param):
+    return param*3
+
+def square(param):
+    return param**2
 
 def main():
-    pass
+
+    for i in range(1,11):
+        sq = square(i)
+        tr = triple(i)
+
+        if sq <= tr:
+             print(f'triple({i})=={tr} square({i})=={sq}')
+        else:
+            break
 
 if __name__ == "__main__":
     main()
--- hy-data-analysis-with-python-2020/part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ hy-data-analysis-with-python-2020/part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -2,9 +2,46 @@
 
 import math
 
+# Create a program that can compute the areas of three shapes, triangles,
+# rectangles and circles, when their dimensions are given.
+# An endless loop should ask for which shape you want the area be calculated.
+# An empty string as input will exit the loop. If the user gives a string
+# that is none of the given shapes, the message “unknown shape!”
+# should be printed. Then it will ask for dimensions for that particular shape.
+# When all the necessary dimensions are given, it prints the area, and starts
+# the loop all over again. Use format specifier f for the area.
 
 def main():
     # enter you solution here
+    condition = True
+
+    while condition:
+        shape = input('Choose a shape (triangle, rectangle, circle): ')
+
+        if shape=='':
+            condition = False
+
+        elif shape=='triangle':
+            base = float(input('Give base of the triangle: ' ))
+            height = float(input('Give height of the triangle: '))
+            area = base*height/2
+            print('The area is', '{:6f}'.format(area))
+
+        elif shape=='rectangle':
+            base = float(input('Give base of the rectangle: ' ))
+            height = float(input('Give height of the rectangle: '))
+            area = base*height
+            print('The area is', '{:6f}'.format(area))
+
+        elif shape=='circle':
+            r = float(input('Give radius of the circle: '))
+            area = math.pi*r**2
+            print('The area is', '{:6f}'.format(area))
+
+        else:
+            print(f'Unknown shape!')
+
+
 
 if __name__ == "__main__":
     main()
