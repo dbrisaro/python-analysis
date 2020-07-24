95d52d70d35ab518ff2c8093f36cd6823ecf733a
daniu <dbrisaro@gmail.com>
2020-07-23 22:27:09 -0300

    Agrego ejericio 9, apareo O(n)

--- hy-data-analysis-with-python-2020/part01-e09_merge/src/merge.py
+++ hy-data-analysis-with-python-2020/part01-e09_merge/src/merge.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 
-def merge(L1, L2):
-    return []
+def merge(L1, L2):          # apareo
+    i = 0
+    j = 0
+    l1 = len(L1)
+    l2 = len(L2)
+    L = [0] * (l1+l2)
+    for k in range(l1+l2):
+        if (j>=l2 or (i<l1 and L1[i]<L2[j])):
+            L[k] = L1[i]
+            i = i+1
+        else:
+            L[k] = L2[j]
+            j = j+1
+    return L
 
 def main():
-    pass
+    L1 = [-63, -62, -57, -51, -26, -22, -21, 4, 16, 35, 35, 38, 43, 56, 63, 65, 66, 74, 93, 99]
+    L2 = [-72, -41, -20, -19, -8, 8, 20, 35, 58, 80]
+    L = merge(L1,L2)
+    print(L)
 
 if __name__ == "__main__":
     main()
